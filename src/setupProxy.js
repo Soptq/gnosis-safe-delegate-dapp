14938563ffaaba0c2f26a28bccac83405d41fa55
kx9x <kx9x@protonmail.com>
2022-01-24 15:13:39 -0800

    feat: fantom support

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,8 @@
 // App is an express application, we can add an express middleware that will set headers for manifest.json request
 // https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function (app) {
     app.use("/manifest.json", function (req, res, next) {
         res.set({
@@ -11,5 +13,16 @@ module.exports = function (app) {
         })
 
         next()
-    })
+    });
+
+    app.use(
+        '/fantom',
+        createProxyMiddleware({
+          target: 'https://safe.fantom.network/',
+          changeOrigin: true,
+          pathRewrite: {
+              [`^/fantom`]: '',
+          }
+        })
+      );
 }
